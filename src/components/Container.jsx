a840b03ee0a3e6db4073227f91071a4ca5e3655d
unknown <john.muchiri@student.moringaschool.com>
2024-08-08 18:42:55 +0300

    project

---
+++ src/components/Container.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import TransactionList from "./TransactionList";
+import Search from "./Search";
+import TransactionForm from "./TransactionForm";
+
+function Container() {
+  const [transaction, setTransaction] = useState([])
+  const [query, setQuery] = useState("")
+  useEffect(() => {
+    fetch("http://localhost:3000/transactions?q=" + query )
+      .then((resp) => resp.json())
+      .then(transaction => setTransaction(transaction))
+  }, [query])
+  
+  function handleSearch(e) {
+    setQuery(e.target.value)
+  }
+  return (
+    <div>
+      <Search handleSearch={handleSearch} />
+      <TransactionForm />
+      <TransactionList transactions={transaction} />
+    </div>
+  );
+}
+
+export default Container;
